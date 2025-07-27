e3772435acbd4adeb838ac649e0e5addb0d3c19e
Mahesh Mani <manimahesh1@gmail.com>
2025-07-27 16:54:55 -0700

    Before DAO

---
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.codewithmosh.store.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repositories.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping()
+    public Iterable<User> getAllUsers() {
+        // Logic to retrieve all users
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        // Logic to retrieve a user by ID
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
+    }
+
+}
