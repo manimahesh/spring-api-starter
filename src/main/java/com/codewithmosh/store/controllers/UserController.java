77c9176e6f5ab3c4d30cd64a0a9c9767fea6243b
Mahesh Mani <manimahesh1@gmail.com>
2025-07-28 16:01:12 -0700

    JPA

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 
@@ -18,19 +19,23 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping()
-    public Iterable<User> getAllUsers() {
+    public Iterable<UserDto> getAllUsers() {
         // Logic to retrieve all users
-        return userRepository.findAll();
+        return userRepository.findAll()
+        .stream()
+        .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+        .toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
         // Logic to retrieve a user by ID
         var user = userRepository.findById(id).orElse(null);
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(user);
+        UserDto userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 
 }
